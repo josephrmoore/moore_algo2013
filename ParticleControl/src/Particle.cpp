a078048c727565c97822eb1aae03a663e59bd828
josephrmoore <josephrmoore@gmail.com>
2013-09-30 16:42:19 -0400

    hw week 5

---
+++ ParticleControl/src/Particle.cpp
@@ -0,0 +1,9 @@
+//
+//  Particle.cpp
+//  ParticleControl
+//
+//  Created by Joseph Moore on 9/28/13.
+//
+//
+
+#include "Particle.h"
---
+++ ParticleControl/src/Particle.h
@@ -0,0 +1,14 @@
+//
+//  Particle.h
+//  ParticleControl
+//
+//  Created by Joseph Moore on 9/28/13.
+//
+//
+
+#ifndef __ParticleControl__Particle__
+#define __ParticleControl__Particle__
+
+#include <iostream>
+
+#endif /* defined(__ParticleControl__Particle__) */
